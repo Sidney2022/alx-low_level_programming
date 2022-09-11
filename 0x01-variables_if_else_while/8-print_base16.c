9c8182d56ecd6f19e52624c225f40b5dcaf18bed
Sidney2022 <uwayasidney@gmail.com>
2022-09-11 23:30:34 +0000

    updated

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,19 +3,25 @@
 /**
  * main - main function
  *
- * Description: a program that prints all the numbers of base 16 in lowercase
+ * Description:  a program that prints all the numbers of base 16 in lowercase
  *
  * Return: 0
  */
-
 int main(void)
 {
 	char c;
+	int num;
 
-	for (c = 'z'; c >= 'a'; c--)
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num%10) + '0');
+	}
+
+	for (c = 'a' ; c <= 'f' ; c++)
 	{
 		putchar(c);
 	}
+
 	putchar('\n');
 	return (0);
 }
