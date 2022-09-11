d13c42268f01b4cac925d8c5c4286ee6e345173f
Sidney2022 <uwayasidney@gmail.com>
2022-09-11 17:41:18 +0000

    usimg the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: program to print certain a string using the 'puts()' function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char myStr[] = "Programming is like building a multilingual puzzle";
+
+	puts(myStr);
+	return (0);
+}
