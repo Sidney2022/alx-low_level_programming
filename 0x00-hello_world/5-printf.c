dc14d92341e2e66f06c3ff73cf2475b1fee5d48f
Sidney2022 <uwayasidney@gmail.com>
2022-09-11 17:50:07 +0000

    usimg the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: program to print certain a string using the printf function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char myStr[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	puts(myStr);
+	return (0);
+}
