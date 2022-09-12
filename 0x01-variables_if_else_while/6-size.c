1d37cfc359899e51e71a3ab150c505d6faf634a0
Sidney2022 <uwayasidney@gmail.com>
2022-09-12 01:28:38 +0000

    committed

---
+++ 0x01-variables_if_else_while/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of an int: %ld byte(s)\n", sizeof(int));
+	printf("size of long int: %ld byte(s)", sizeof(long int));
+	return (0);
+}
