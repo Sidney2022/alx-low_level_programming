a6edc3ab05eea179a3e7062e907d9461496bb78b
Sidney2022 <uwayasidney@gmail.com>
2022-09-12 03:54:20 +0000

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: program to print a sequence of characters
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char myStr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	puts(myStr);
+	return (1);
+}
