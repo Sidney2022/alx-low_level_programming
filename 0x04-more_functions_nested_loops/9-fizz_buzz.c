e490bc1ae20bcaced9a2eed27fda9980d99b5ab7
Sidney2022 <uwayasidney@gmail.com>
2022-09-16 13:14:22 +0000

    fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - function to print output of fizzbuzz challenge
+ *
+ * Return: fizz -if number is a multiple of 3
+ * - buzz -if number is a multiple of 5
+ * - fizzbuzz -if number is a multiple of 15
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
